1aed1a1bd1bc814aafe980590dac97473b189f36
Leif Sawyer <ak.hepcat@gmail.com>
2019-05-20 11:35:34 -0800

    Add the listing of available logs displayed on no-choice

--- plog.sh
+++ plog.sh
@@ -5,16 +5,26 @@ COLUMNS=$(stty size | awk '{print $2}')
 if [ -z "$1" ]
 then
 	echo "which host? "
-	/bin/ls -C --width=${COLUMNS} *.log 2>/dev/null| sed 's/\.log//gi;'
+	/bin/ls -C --width=${COLUMNS} *.log 2>/dev/null | sed 's/\.log//gi;'
 	exit 1
 fi
 
 if [ -r "$1" ]
 then
-	MARK=$(grep MARK "${1}" | awk '{print $2}' | tail -1)
-
-	sed -n "/${MARK}/,\$p" "${1}"
+	LF=${1}
+elif [ -r "${1}.log" ]
+then
+	LF="${1}.log"
 else
-	echo "can't open logfile ${1}"
+	LF=""
+fi
+
+if [ -n "${LF}" ]
+then
+	MARK=$(grep MARK "${LF}" | awk '{print $2}' | tail -1)
+
+	sed -n "/${MARK}/,\$p" "${LF}"
+else
+	echo "can't open logfile ${LF:-$1}"
 	exit 1
 fi
